2a6bd238abb3cdfc0d1439ed2218f814fc61c649
ferico <huangferico@gmail.com>
2025-10-29 13:22:52 +0700

    add about section

---
+++ components/AnimatedTitle.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+type AnimatedTitleProps = {
+  title: string;
+  containerClass?: string;
+};
+
+export const AnimatedTitle = ({ title, containerClass }: AnimatedTitleProps) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: '100 bottom',
+          end: 'center bottom',
+          toggleActions: 'play none none reverse',
+        },
+      });
+
+      titleAnimation.to(
+        '.animated-word',
+        {
+          opacity: 1,
+          transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
+          ease: 'power2.inOut',
+          stagger: 0.02,
+        },
+        0
+      );
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+      {title.split('<br />').map((line: string, index: number) => (
+        <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+          {line.split(' ').map((word: string, i: number) => (
+            <span key={i} className="animated-word" dangerouslySetInnerHTML={{ __html: word }}></span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
